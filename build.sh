62ac08c9910a7e2e5d558a88d5e6f4ab4736c75e
Robin Swift <robinswiftart@gmail.com>
2024-04-11 23:21:49 +0100

    refman/ libembroidery/

--- build.sh
+++ build.sh
@@ -3,21 +3,36 @@
 # Lighter weight hugo/jekyll style static site generator.
 
 function make_page () {
-pandoc content/$1.md -o content/$1.html
-cat head.html content/$1.html foot.html > _site/$1.html
+	pandoc content/$1.md -o content/$1.html
+	cat head.html content/$1.html foot.html > _site/$1.html
 }
 
 function update_refman () {
-wget https://raw.githubusercontent.com/embroidermodder/refman/main/emrm.pdf
-mv emrm.pdf downloads/emrm-current.pdf
+	git submodule init
+	git submodule update
+	wget https://raw.githubusercontent.com/embroidermodder/refman/main/emrm.pdf
+	mv emrm.pdf downloads/emrm-current.pdf
 }
 
-rm -fr _site
-mkdir -p _site
-cp -R data downloads commands images icons samples fonts favicon.png _site
+function build_emscripten_version () {
+	git submodule init
+	git submodule update
+	cd libembroidery
+	emcc embroidery.c -o embroidery.wasm
+	mv embroidery.wasm ../downloads
+}
 
-for file in index downloads about changelog credits documentation todo converter
-do
-make_page $file
-done
+function build_website () {
+	rm -fr _site
+	mkdir -p _site
+	cp -R data downloads commands images icons samples fonts favicon.png _site
 
+	for file in index downloads about changelog credits documentation todo converter
+	do
+		make_page $file
+	done
+}
+
+#update_refman
+#build_emscripten_version
+build_website
