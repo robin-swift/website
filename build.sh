1fb69ffdd26a6ab164413b4adc02c2ec24ac0865
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2024-04-26 08:06:39 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,6 @@ function make_page () {
 	cat head.html content/$1.html foot.html > _site/$1.html
 }
 
-function update_refman () {
-	git submodule init
-	git submodule update
-	wget https://raw.githubusercontent.com/embroidermodder/refman/main/emrm.pdf
-	mv emrm.pdf downloads/emrm-current.pdf
-}
-
 function build_emscripten_version () {
 	git submodule init
 	git submodule update
@@ -22,20 +15,26 @@ function build_emscripten_version () {
 	mv embroidery.wasm ../downloads
 }
 
-function build_website () {
-	git submodule init
-	git submodule update
-
- 	rm -fr _site
-	mkdir -p _site
-	cp -R refman/data downloads commands images icons samples fonts favicon.png _site
-
-	for file in index downloads about changelog credits documentation todo converter
-	do
-		make_page $file
-	done
+function build_subproject () {
+	cd $1
+		bash build.sh
+	cd ..
+	cp -R $1/_site _site/$1
 }
 
-#update_refman
 #build_emscripten_version
-build_website
+
+git submodule init
+git submodule update
+
+rm -fr _site
+mkdir -p _site
+cp -R refman/data downloads commands images icons samples fonts favicon.png _site
+
+for file in index downloads about changelog credits documentation todo converter
+do
+	make_page $file
+done
+
+build_subproject refman
+build_subproject user-manual
