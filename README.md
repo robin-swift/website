00f880b3b939682cf195ebe717f54ed9ce624f17
Robin Swift <robinswiftart@gmail.com>
2024-09-23 21:01:21 +0100

    status badges, 755 mode scripts

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Embroidermodder Project Website
 
+[![Deploy website](https://github.com/Embroidermodder/website/actions/workflows/website.yml/badge.svg)](https://github.com/Embroidermodder/website/actions/workflows/website.yml)
+[![Testing](https://github.com/Embroidermodder/website/actions/workflows/test_report.yml/badge.svg)](https://github.com/Embroidermodder/website/actions/workflows/test_report.yml)
+
 This directory is under GNU FDL 1.3 except for large code quotations which are under the license [here](https://github.com/embroidermodder/embroidermodder).
--- test.sh
+++ test.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-mkdir test_report
+mkdir test_report || exit 1
 
-git clone https://github.com/Embroidermodder/embroidermodder
-cd embroidermodder
-bash build.sh --linux &> ../test_report/build_embroidermodder.log
-bash build.sh -d &> ../test_report/debug_embroidermodder.log
-cd ..
+git clone https://github.com/Embroidermodder/embroidermodder || exit 1
+cd embroidermodder || exit 1
+bash build.sh --linux &> ../test_report/build_embroidermodder.log || exit 1
+bash build.sh -d &> ../test_report/debug_embroidermodder.log || exit 1
+cd .. || exit 1
 
-git clone https://github.com/Embroidermodder/libembroidery
-cd libembroidery
-bash build.sh -d &> ../test_report/build_libembroidery.log
-cd debug
-./embroider --test &> ../test_report/test_embroider.log
-cd ..
-cd ..
+git clone https://github.com/Embroidermodder/libembroidery || exit 1
+cd libembroidery || exit 1
+bash build.sh -d &> ../test_report/build_libembroidery.log || exit 1
+cd debug || exit 1
+./embroider --test &> ../test_report/test_embroider.log || exit 1
+cd .. || exit 1
+cd .. || exit 1
 
-# git clone https://github.com/Embroidermodder/EmbroideryMobile
-# cd EmbroideryMobile
-# bash build.sh -d &> ../build_mobile.log
-# cd ..
+git clone https://github.com/Embroidermodder/EmbroideryMobile || exit 1
+cd EmbroideryMobile || exit 1
+# bash build.sh -d &> ../build_mobile.log || exit 1
+cd .. || exit 1
