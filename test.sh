1995c55671ca5c967b3626e42fafe5aa85a97bc3
Robin Swift <robinswiftart@gmail.com>
2024-09-23 22:39:45 +0100

    bugfix

--- test.sh
+++ test.sh
@@ -3,52 +3,53 @@
 RES="`pwd`/docs/testing.md"
 
 function fail_result () {
+    echo -e '\n```\n' >> $RES
     echo "$1" >> $RES
     exit 0
 }
 
+date &>> $RES
+
 echo "## Embroidermodder Download" > $RES
 git clone https://github.com/Embroidermodder/embroidermodder &>> $RES || fail_result 1
-cd embroidermodder || fail_result 2
+cd embroidermodder
 
 echo "## Embroidermodder Build" >> $RES
 echo -e '\n```\n' >> $RES
-bash build.sh --linux &>> $RES || fail_result 3
+bash build.sh --linux &>> $RES || fail_result 2
 echo -e '\n```\n' >> $RES
 
 echo "## Embroidermodder Debug" >> $RES
 echo -e '\n```\n' >> $RES
-bash build.sh -d &>> $RES || fail_result 4
+bash build.sh -d &>> $RES || fail_result 3
 echo -e '\n```\n' >> $RES
 
-cd .. || fail_result 5
+cd ..
 
-git clone https://github.com/Embroidermodder/libembroidery &>> $RES || fail_result 6
-cd libembroidery || fail_result 7
+git clone https://github.com/Embroidermodder/libembroidery &>> $RES || fail_result 4
+cd libembroidery
 
 echo "## Libembroidery Build" >> $RES
 echo -e '\n```\n' >> $RES
-bash build.sh -d &>> $RES || fail_result 8
+cmake . &>> $RES || fail_result 5
+make &>> $RES || fail_result 6
 echo -e '\n```\n' >> $RES
-
-cd debug || fail_result 9
 
 echo "## Libembroidery Testing" >> $RES
 echo -e '\n```\n' >> $RES
-./embroider --test &>> $RES || fail_result 10
+./embroider --test &>> $RES || fail_result 7
 echo -e '\n```\n' >> $RES
 
-cd .. || fail_result 11
-cd .. || fail_result 12
+cd ../..
 
-git clone https://github.com/Embroidermodder/EmbroideryMobile || fail_result 13
-cd EmbroideryMobile || fail_result 14
+git clone https://github.com/Embroidermodder/EmbroideryMobile || fail_result 8
+cd EmbroideryMobile
 
 echo "## " >> $RES
 echo -e '\n```\n' >> $RES
-bash build.sh -d &>> $RES || fail_result 15
+bash build.sh -d &>> $RES || fail_result 9
 echo -e '\n```\n' >> $RES
 
-cd .. || fail_result 16
+cd ..
 
 echo "0" >> $RES
