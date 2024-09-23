fe713e12aa93af8538732d1fba4048890d98bdc2
Robin Swift <robinswiftart@gmail.com>
2024-09-23 21:22:02 +0100

    produce report on fail

--- test.sh
+++ test.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
 mkdir test_report || exit 1
+echo "0" > test_report/result.txt
 
-git clone https://github.com/Embroidermodder/embroidermodder || exit 1
-cd embroidermodder || exit 1
-bash build.sh --linux &> ../test_report/build_embroidermodder.log || exit 1
-bash build.sh -d &> ../test_report/debug_embroidermodder.log || exit 1
-cd .. || exit 1
+function fail_result () {
+    echo "$1" > test_report/result.txt
+    exit 0
+}
 
-git clone https://github.com/Embroidermodder/libembroidery || exit 1
-cd libembroidery || exit 1
-bash build.sh -d &> ../test_report/build_libembroidery.log || exit 1
-cd debug || exit 1
-./embroider --test &> ../test_report/test_embroider.log || exit 1
-cd .. || exit 1
-cd .. || exit 1
+git clone https://github.com/Embroidermodder/embroidermodder || fail_result 1
+cd embroidermodder || fail_result 2
+bash build.sh --linux &> ../test_report/build_embroidermodder.log || fail_result 3
+bash build.sh -d &> ../test_report/debug_embroidermodder.log || fail_result 4
+cd .. || fail_result 5
 
-git clone https://github.com/Embroidermodder/EmbroideryMobile || exit 1
-cd EmbroideryMobile || exit 1
-# bash build.sh -d &> ../build_mobile.log || exit 1
-cd .. || exit 1
+git clone https://github.com/Embroidermodder/libembroidery || fail_result 6
+cd libembroidery || fail_result 7
+bash build.sh -d &> ../test_report/build_libembroidery.log || fail_result 8
+cd debug || fail_result 9
+./embroider --test &> ../test_report/test_embroider.log || fail_result 10
+cd .. || fail_result 11
+cd .. || fail_result 12
+
+git clone https://github.com/Embroidermodder/EmbroideryMobile || fail_result 13
+cd EmbroideryMobile || fail_result 14
+bash build.sh -d &> ../test_report/build_mobile.log || fail_result 15
+cd .. || fail_result 16
