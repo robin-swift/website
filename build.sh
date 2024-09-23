a95db70547c5e71d58b429e91ed7f9cdb7d23aab
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2024-09-23 22:03:35 +0100

    build bug

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "```" > docs/testing.md
+echo '```' > docs/testing.md
 cat test_report/* >> docs/testing.md
-echo "```" >> docs/testing.md
+echo '```' >> docs/testing.md
 
 git clone https://github.com/embroidermodder/userman
 git clone https://github.com/embroidermodder/refman
